acd647c32a2c47f8a774078b51cea547a914d304
Lucas Pacheco <devpacheco7@gmail.com>
2026-03-06 16:29:49 -0300

    habilitação comentários xml

--- Gerenciador-de-tarefas.API/Program.cs
+++ Gerenciador-de-tarefas.API/Program.cs
@@ -6,10 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+//Comentários xml
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
+//Conexao banco de dados
 builder.Services.AddDbContext<TaskDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
